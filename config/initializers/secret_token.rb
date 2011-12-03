091a6a75df61c7d198bc1581f1e99a3a9a3904f6
Al Shaw <almshaw@gmail.com>
2011-12-03 11:22:44 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tohack11::Application.config.secret_token = 'a1be3a04b0e39f0ce2e553792bff151099e96090b2fe3186cda69685954987e208b643edf294515da6cf3428aa44d424d997fc281e8800f38ac9292eab8690ff'
