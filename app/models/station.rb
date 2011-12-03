44507760e599520667e3691ed888f7d4538d3ae8
Erik Hinton <erikhinton@Eriks-MacBook-Air.local>
2011-12-03 16:34:12 -0500

    View logic

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,11 @@
 class Station < ActiveRecord::Base
   has_many :tweets
   has_and_belongs_to_many :lines  
+
+  def get_happiness
+      happy = self.tweets.where(:happiness_string => "good").count
+      sad = self.tweets.where(:happiness_string => "bad").count
+      [happy,sad]
+  end
+
 end
