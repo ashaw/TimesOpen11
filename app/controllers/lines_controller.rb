32aeb7fe47fb58c3a3d1aa2f71a685a96b0990bc
Julian Burgess <aubergene@gmail.com>
2011-12-03 20:16:17 -0500

    beginings of the thing

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,5 +1,9 @@
 class LinesController < ActionController::Base
 
+  def index
+    @lines = Line.order(:name).all.to_json
+  end
+
   def show
     @line = Line.find_by_name(params[:name])
     @stations = @line.stations.group("station_name").order("latitude DESC").order("longitude DESC")
